17383f707d8fa1bb653e3439d5b4162f96540668
Yegor <yegorh8@gmail.com>
2021-03-02 16:56:19 +0200

    initial commit

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this is the scream body',
+      createdAt: '2021-03-02T08:33:53.195Z',
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+};

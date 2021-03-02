5c4b1c4bf043ecc1c4469dba01623079265cb15b
Yegor <yegorh8@gmail.com>
2021-03-02 16:58:28 +0200

    Create config.js

---
+++ functions/util/config.js
@@ -0,0 +1,11 @@
+
+
+module.exports = {
+    apiKey: "AIzaSyDOELDm4fR-qDga2FurLviVxSLSn3eKsxk",
+    authDomain: "foodie-ebd21.firebaseapp.com",
+    databaseURL: "https://foodie-ebd21-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "foodie-ebd21",
+    storageBucket: "foodie-ebd21.appspot.com",
+    messagingSenderId: "194148596679",
+    appId: "1:194148596679:web:2b124d117fa1c3952b37e2"
+}

0436ee1411619cb1399fc3726ffdadf04ef1d42e
Yegor Horuzhenko <yegorh8@gmail.com>
2021-11-08 18:03:38 +0200

    update

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const auth = getAuth();
+export const auth = getAuth(app);
 
 
 export function signup(email, password) {
